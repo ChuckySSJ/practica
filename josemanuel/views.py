7b0f73694ee761643b539ea9fdfb0177d733d614
ChuckySSJ <ChuckySSJ.pepito_chivaz10@hotmail.com>
2021-04-07 14:14:48 -0500

    version 1

---
+++ josemanuel/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
+
+# Create your views here.
+
+def josemanuel(request):
+		externo = open("C:/Users/Chucky/Desktop/josemanuellopez/templates/index.html")
+		plantilla = Template(externo.read())
+		externo.close()
+		ctx = Context()
+		contenido = plantilla.render(ctx)
+		return HttpResponse(contenido)
