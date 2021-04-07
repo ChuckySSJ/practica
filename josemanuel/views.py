3957e6d3cf872fd7bf4d6a65b0d2b3c9d848b3c0
ChuckySSJ <ChuckySSJ.pepito_chivaz10@hotmail.com>
2021-04-07 15:01:21 -0500

    version 3 ultima

--- josemanuel/views.py
+++ josemanuel/views.py
@@ -5,7 +5,7 @@ from django.template import Template,Context
 # Create your views here.
 
 def josemanuel(request):
-		externo = open("C:/Users/Chucky/Desktop/josemanuellopez/templates/index.html")
+		externo = open("C:/Users/Chucky/Desktop/josemanuellopez/josemanuel/templates/index.html")
 		plantilla = Template(externo.read())
 		externo.close()
 		ctx = Context()
